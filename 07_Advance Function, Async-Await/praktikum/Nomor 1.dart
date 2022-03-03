e265f69f5028982778783667fd8c3eb81459b809
Naufal Ulwan <75620422+opangpoo123@users.noreply.github.com>
2022-03-03 23:54:09 +0700

    Menambahkan tugas dan resume ke section 7

---
+++ 07_Advance Function, Async-Await/praktikum/Nomor 1.dart
@@ -0,0 +1,20 @@
+void main() async {
+  List list_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  int pengali_input = 3;
+  var result = await listKali(list_input, pengali_input);
+  print(result);
+}
+
+listKali(List list_data, int pengali) async {
+  List new_data = [];
+
+  for (var item in list_data) {
+    int temp_num = await pengalian(item, pengali);
+    new_data.add(temp_num);
+  }
+  return new_data;
+}
+
+pengalian(int _item, int _pengali) {
+  return _item * _pengali;
+}
