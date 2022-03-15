27af23a3cb14216add6a5342d652abb7061114cb
Naufal Ulwan <75620422+opangpoo123@users.noreply.github.com>
2022-03-15 11:52:42 +0700

    Tugas keupdate

--- 10_Dart Object Oriented Programming 2/praktikum/Implements.dart
+++ 10_Dart Object Oriented Programming 2/praktikum/Implements.dart
@@ -2,7 +2,7 @@ import 'class_Matematika.dart';
 
 void main() {
   Matematika operation = new Matematika();
-  operation.x2 = 8;
-  operation.y2 = 6;
+  operation.x2 = 105;
+  operation.y2 = 135;
   print(operation.hasil());
 }
