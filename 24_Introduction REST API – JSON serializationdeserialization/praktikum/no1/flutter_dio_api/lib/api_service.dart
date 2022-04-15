e2a5a513a3223719fd9c37f993be6a159b95717d
Naufal Ulwan <75620422+opangpoo123@users.noreply.github.com>
2022-04-15 20:38:48 +0700

    Menambahkan sebuah tugas praktikum dan hasilnya

---
+++ 24_Introduction REST API – JSON serializationdeserialization/praktikum/no1/flutter_dio_api/lib/api_service.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dio_api/model.dart';
+
+class ApiService {
+  final String _url =
+      'https://my-json-server.typicode.com/hadihammurabi/flutter-webservice/contacts/';
+
+  late Dio _dio;
+
+  ApiService() {
+    _dio = Dio();
+  }
+
+  Future getData() async {
+    Response response = await _dio.get(_url);
+    if (response.statusCode == 200) {
+      Iterable it = response.data;
+      List<Contact> contact = it.map((e) => Contact.fromJson(e)).toList();
+      return contact;
+    }
+  }
+
+  Future<Contact> postData(String name, String phone) async {
+    Response response =
+        await _dio.post(_url, data: {'name': name, 'phone': phone});
+
+    return Contact.fromJson(response.data);
+  }
+}
---
+++ 24_Introduction REST API – JSON serializationdeserialization/praktikum/no1/flutter_dio_api/lib/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dio_api/api_service.dart';
+
+import '../model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ApiService service = ApiService();
+  List<Contact> listContact = [];
+
+  getData() async {
+    listContact = await service.getData();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ApiService().postData('Naufal Ulwan', '082124293938').then((value) {
+            listContact.add(value);
+            setState(() {});
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Daftar Kontak',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: listContact.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(listContact[index].name),
+                        subtitle: Text(listContact[index].phone),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
